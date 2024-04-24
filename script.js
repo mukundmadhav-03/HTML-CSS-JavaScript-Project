779aa9546de4796ddbaa467a8d30367025b9abf2
Mukund Madhav <mukundmadhav1998@gmail.com>
2024-04-24 21:59:06 +0530

    Initial Files

---
+++ script.js
@@ -0,0 +1,24 @@
+let hour=document.getElementById("hour")
+let min=document.getElementById("min")
+let sec=document.getElementById("sec")
+
+function setDail(){
+    let date= new Date()
+    let hh=date.getHours()
+    let mm=date.getMinutes()
+    let ss=date.getSeconds()
+
+
+   //method of rotation
+    let hRotate=30*hh + 0.5*mm;
+    let mRotate=6*mm;
+    let sRotate=6*ss;
+
+    //style
+
+    hour.style.transform=`rotate(${hRotate}deg)`
+    min.style.transform=`rotate(${mRotate}deg)`
+    sec.style.transform=`rotate(${sRotate}deg)`
+}
+
+setInterval(setDail,1000);
